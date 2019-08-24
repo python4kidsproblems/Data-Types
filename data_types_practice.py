9dd78bdd41812ed233547ec665becf8a25c3bdb2
python4kidsproblems <54461624+python4kidsproblems@users.noreply.github.com>
2019-08-24 09:03:16 +0500

    Add files via upload

---
+++ data_types_practice.py
@@ -0,0 +1,34 @@
+# Data Types Practice Problems
+
+# How to use this file:
+# Copy this to your python (.py) file, and write your code in the given space
+
+################################### Q1 ###################################
+# Write a program that that takes user age as string, and converts the   #
+# the fist character and last character of the string to ints and prints #
+# them.                                                                  #
+# e.g "17" -> 1, 7                                                       #
+# make sure the resulting numbers are ints by using the type() function  #
+##########################################################################
+
+# your code goes here
+
+################################### Q2 ###################################
+# Write a program that asks the user,                                    #
+# "Are you older than 15? Answer True or Flase."                         #
+# Convert the user response to Bool and then check its type using the    #
+# type function.                                                         #
+# See what happpends if the user does not repond with True or False,     #
+# and replies with Yes or No instead. Will this program still work?      #
+##########################################################################
+
+# your code goes here
+
+################################### Q2 ###################################
+# Make a variable and store any 3 digit number in it.                    #
+# Your task is to reverse the 3 digits,                                  #
+# e.d 672 -> 276                                                         #
+# Hint, try converting to another data type.                             #
+##########################################################################
+
+# your code goes here
